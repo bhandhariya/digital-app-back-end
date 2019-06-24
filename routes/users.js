fc4b48bb6ab23014f4b78d4a447e30ad85592a9a
bhandhariya <sainiraja59@gmail.com>
2019-06-24 13:14:27 +0530

    test api created

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -8,7 +9,9 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/test', function(req, res, next) {
-  res.send(req.body);
+ var data=req.body;
+ console.log(data)
+
 });
 
 
