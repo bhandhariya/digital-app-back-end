cb08b64b3ae2394713805d65cb7bd5850406188c
bhandhariya <sainiraja59@gmail.com>
2019-06-24 13:18:18 +0530

    mongodb url changed

--- db.js
+++ db.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 
 //  var mongoURI = "mongodb://localhost:27017/digital-App-1";
 
-var mongoURI = "mongodb://raja:mentcom123@ds147096.mlab.com:47096/digitalapp";
+// var mongoURI = "mongodb://raja:mentcom123@ds147096.mlab.com:47096/digitalapp";
 
 // var mongoURI = "mongodb://localhost:27017/digital-App-2";
+
+var mongoURI = "mongodb://rajasaini:digital123@ds341847.mlab.com:41847/newdigitalapp";
 
 
 mongoose.Promise = global.Promise;
