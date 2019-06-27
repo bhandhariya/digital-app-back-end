58599a781efe7518c4305d4c215579edefe04f56
bhandhariya <sainiraja59@gmail.com>
2019-06-27 15:20:04 +0530

    patient model ready

--- model/patient_model.js
+++ model/patient_model.js
@@ -3,21 +3,23 @@ var Schema = mongoose.Schema;
 
 var PatientSchema = new Schema({
     _id:{type:Schema.Types.ObjectId,required:true},
+    doctorName:{type:Schema.Types.String,required:true},
     addmission_date:{type:Schema.Types.Date},
     first_name:{type:Schema.Types.String},
     middle_name:{type:Schema.Types.String},
     last_name:{type:Schema.Types.String},
-    profile_image:{type:Schema.Types.String,required:true},
+    profile_image:{type:Schema.Types.String},
     permanent_address:{type:Schema.Types.String},
-    permanent_mobile:{type:Schema.Types.Number},
-    permanent_fax:{type:Schema.Types.Number},
+    permanent_mobile:{type:Schema.Types.Number,required:true,unique:true},
+    // permanent_fax:{type:Schema.Types.Number},
     permanent_residance_number:{type:Schema.Types.Number},
     permanent_landline:{type:Schema.Types.Number},
     correspondence_address:{type:Schema.Types.String},
-    correspondence_mobile:{type:Schema.Types.Number},
-    correspondence_fax:{type:Schema.Types.Number},
-    correspondence_residance_number:{type:Schema.Types.Number},
-    correspondence_landline:{type:Schema.Types.Number},
+    // correspondence_mobile:{type:Schema.Types.Number},
+    // correspondence_fax:{type:Schema.Types.Number},
+    // correspondence_residance_number:{type:Schema.Types.Number},
+    // correspondence_landline:{type:Schema.Types.Number},
+    officeNumber:{type:Schema.Types.Number},
     email:{type:Schema.Types.String},
     DOB:{type:Schema.Types.Date},
     age:{type:Schema.Types.Number},
@@ -25,16 +27,22 @@ var PatientSchema = new Schema({
     marrital_status:{type:Schema.Types.String},
     education:{type:Schema.Types.String},
     occupation:{type:Schema.Types.String},
+
+    ///done
+    father_name:{type:Schema.Types.String},
+    father_address:{type:Schema.Types.String},
+    father_number:{type:Schema.Types.Number},
+    father_email:{type:Schema.Types.String},
+    guardian_name:{type:Schema.Types.String},
+    guardian_contact_number:{type:Schema.Types.Number},
+    guardian_email:{type:Schema.Types.String},
+    guardian_address:{type:Schema.Types.String},
     spouse_name:{type:Schema.Types.String},
     spouse_age:{type:Schema.Types.Number},
     spouse_occupation:{type:Schema.Types.String},
     spouse_address:{type:Schema.Types.String},
     spouse_relation_status:{type:Schema.Types.String},
-    no_of_childern:{type:Schema.Types.Number},
-    guardian_name:{type:Schema.Types.String},
-    guardian_contact_number:{type:Schema.Types.Number},
-    guardian_email:{type:Schema.Types.String},
-    guardian_address:{type:Schema.Types.String}
+    no_of_childern:{type:Schema.Types.Number}
 
 
 
